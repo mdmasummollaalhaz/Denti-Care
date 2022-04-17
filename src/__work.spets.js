64e7c32766d989ce98a16e78e9f242e79262d8f6
Masum Mollo <freelancermasummolla@gmail.com>
2022-04-17 19:39:04 +0600

    react bootstrap and bootstrap css added

--- src/__work.spets.js
+++ src/__work.spets.js
@@ -4,4 +4,5 @@
  * 3. create a new project at console.google.com
  * 4. npm install firebase
  * 5. create firebase.init.js and .env.local
+ * 6. npm install react-bootstrap bootstrap and bootsrap css added
  */
