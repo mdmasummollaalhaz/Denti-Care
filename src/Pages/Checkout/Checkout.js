aebc31b1764ac75bf26cfe1b3224248cc0c36d5c
Masum Mollo <freelancermasummolla@gmail.com>
2022-04-18 14:12:46 +0600

    Checkout and service details updated

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,12 +1,83 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import SocialLogin from '../Login/SocialLogin/SocialLogin';
 import './Checkout.css';
 
 const Checkout = () => {
-    return (
-        <div>
-            <h2>Check out</h2>
+  return (
+    <div className="container form-box mx-auto register-form">
+      <h2 style={{ textAlign: 'center' }} className="title">
+        Billing Details
+      </h2>
+      <form>
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Name"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Address"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Town/city"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="State"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Zip"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Phone"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Email"
+            required
+          />
+        </Form.Group>
+        <div className='radioBtn'>
+          <input type="radio"/> Bkash
+          <input type="radio"/> Hand-Cash
         </div>
-    );
+        <input
+          className="w-50 mx-auto btn btn primary mt-3"
+          type="submit"
+          value="Confirm!"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Checkout;
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -3,27 +3,34 @@ import { Link, useParams } from 'react-router-dom';
 import './ServiceDetail.css';
 
 const ServiceDetail = () => {
-    const { serviceId } = useParams();
+  const { serviceId } = useParams();
 
-    const [services, setServices] = useState([]);
+  const [services, setServices] = useState([]);
 
-    useEffect(() =>{
-        fetch('/services.json')
-            .then(res => res.json())
-            .then(data => setServices(data[serviceId-1]));
-    }, []);
-    console.log(services)
-    return (
-        <div className="service">
-      <img className="img-fluid" src={services.img} alt="" />
-      <div className='details'>
-        <h2>{services.name}</h2>
-        <p>Price:{services.price}</p>
-        <p>{services.description}</p>
-        <Link className="book"  to='/checkout' > checkout</Link>
+  useEffect(() => {
+    fetch('/services.json')
+      .then(res => res.json())
+      .then(data => setServices(data[serviceId - 1]));
+  }, []);
+  console.log(services);
+  return (
+    <div className="container serviceDetails">
+      <div className="row">
+        <div className="col-md-6">
+          <img className="img-fluid" src={services.img} alt="" />
+        </div>
+        <div className="col-md-6 details">
+          <h2>{services.name}</h2>
+          <p>Price:{services.price}</p>
+          <p>{services.description}</p>
+          <Link className="btn" to="/checkout">
+            {' '}
+            checkout
+          </Link>
+        </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default ServiceDetail;
